cc806379c7e01dfb8c31e06e63b9a05b1db5ebdc
jraebrown <me@jraebrown.com>
2024-11-30 00:07:50 +0000

    Update post-pve-install.sh
    Quick and dirty have to remove the version check

--- misc/post-pve-install.sh
+++ misc/post-pve-install.sh
@@ -180,12 +180,12 @@ while true; do
   esac
 done
 
-if ! pveversion | grep -Eq "pve-manager/8.[0-2]"; then
-  msg_error "This version of Proxmox Virtual Environment is not supported"
-  echo -e "Requires Proxmox Virtual Environment Version 8.0 or later."
-  echo -e "Exiting..."
-  sleep 2
-  exit
-fi
+#if ! pveversion | grep -Eq "pve-manager/8.[0-2]"; then
+#  msg_error "This version of Proxmox Virtual Environment is not supported"
+#  echo -e "Requires Proxmox Virtual Environment Version 8.0 or later."
+#  echo -e "Exiting..."
+#  sleep 2
+#  exit
+#fi
 
 start_routines
